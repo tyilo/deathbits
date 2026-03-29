1dab97847f75f2c51d91665d4d183a641ea1eb94
Asger Hautop Drewsen <asger@tyilo.com>
2026-03-30 00:45:55 +0200

    Add benchmarks

---
+++ stats/benches/bench.rs
@@ -0,0 +1,25 @@
+use std::time::Duration;
+
+use deathbits::{DiceSumOutcomes, FromRatio, Num};
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench(
+    types = [
+        f64,
+        num_bigint::BigUint,
+        arpfloat::Float,
+        fast_posit::p64,
+        fast_posit::Posit<64, 20, i64>,
+        deathbits::LogNum<f32>,
+        deathbits::LogNum<f64>,
+    ],
+    args = [5, 10, 15],
+    max_time = Duration::from_secs(10),
+)]
+fn bench<T: Num>(n: u32) -> Vec<f64> {
+    let mut cache = DiceSumOutcomes::<T>::new();
+    cache.deathbit_stats(n).into_iter().map(|v| v.as_f64()).collect()
+}
